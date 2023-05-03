f78593be2b167e7ca12fbb871fe79cd540572386
Jps709 <jps7095@psu.edu>
2023-05-03 19:51:51 -0400

    Update 0.4.0

--- api/plan-data.js
+++ api/plan-data.js
@@ -2,15 +2,24 @@ export default async function handler(request, res) {
   const plan = [
     {
       weekNum: '1',
-      compTime: '2 hours to complete',
-      weekTitle: 'Why take this course?',
-      weekDescription: '4 videos (Total 13 min), 11 readings',
+      compTime: '2',
+      weekTitle: 'Introduction',
+      weekDescription: 'Why take this course?',
+      wTotal: '4 videos (Total 13 min), 11 readings',
     },
     {
       weekNum: '2',
-      compTime: '2 hours to complete',
+      compTime: '2',
       weekTitle: 'Misconceptions About Happiness',
       weekDescription: 'What do we think will make us happy?',
+      wTotal: '2 videos (Total 7 min), 2 readings',
+    },
+    {
+      weekNum: '3',
+      compTime: '3',
+      weekTitle: 'Why Our Expectations are so Bad',
+      weekDescription: 'Why do we mispredict what make us happy?',
+      wTotal: '1 videos (Total 4 min), 5 readings',
     },
   ];
 
--- src/suggested-plan.js
+++ src/suggested-plan.js
@@ -1,12 +1,17 @@
 import { LitElement, html, css } from 'lit';
+import './plan-assignment.js';
 
 export class SuggestedPlan extends LitElement {
   static get properties() {
     return {
       weekNum: { type: String },
-      compTime: { type: String },
+      compTime: { type: Number },
       weekTitle: { type: String },
       weekDescription: { type: String },
+      wTotal: { type: String },
+      toggleOpen: { type: Boolean, reflect: true },
+      tTitle: { type: String },
+      tDescription: { type: String },
     };
   }
 
@@ -45,15 +50,36 @@ export class SuggestedPlan extends LitElement {
       font-size: 18px;
       margin: 0px 0px 15px;
     }
+    .details {
+      list-style: none;
+    }
+    @media screen and (min-width: 501px) and (max-width: 800px) {
+      .wrapper {
+        font-size: 10px;
+      }
+    }
   `;
 
   constructor() {
     super();
     this.weekNum = '1';
-    this.compTime = '3 Hours';
+    this.compTime = '3';
     this.weekTitle = 'Misconceptions about happiness';
     this.weekDescription =
       'In this module, you will learn what it means to be happy and why pursuing happiness is not a pointless endeavor. Dr. Santos addresses how our minds lie to us and how the science shows that our misconceptions about money, grades, and social media are holding us back from implementing the techniques studied in positive psychology.';
+    this.display = 'See All';
+    this.wTotal = '4 videos (Total 13 min), 11 readings';
+    this.toggleOpen = false;
+    this.tTitle = 'Video Title';
+    this.tDescription = 'Video Description';
+  }
+
+  toggleEvent() {
+    if (this.shadowRoot.querySelector('details').getAttribute('open') === '') {
+      this.toggleOpen = true;
+    } else {
+      this.toggleOpen = false;
+    }
   }
 
   render() {
@@ -64,9 +90,20 @@ export class SuggestedPlan extends LitElement {
           <p class="wNum">${this.weekNum}</p>
         </div>
         <div class="week-info">
-          <p class="cTime">${this.compTime}</p>
+          <p class="cTime">${this.compTime} hour(s) to complete</p>
           <p class="wTitle">${this.weekTitle}</p>
           <p class="wDescription">${this.weekDescription}</p>
+          <p>${this.wTotal}</p>
+          <details
+            class="details"
+            .open="${this.toggleOpen}"
+            @toggle="${this.toggleEvent}"
+          >
+            <summary>${this.display}</summary>
+            <div class="seeAll">
+              <p>${this.tTitle} ${this.tDescription}</p>
+            </div>
+          </details>
         </div>
       </div>
     `;
